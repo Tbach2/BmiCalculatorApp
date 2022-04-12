71fde817a4d4b0385c8b1675a684be238b72e098
Tyler Bach <Tbach2@my.wctc.edu>
2022-04-11 21:10:44 -0700

    Add project files.

---
+++ BmiCalculatorApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BmiCalculatorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Handle_Clicked(Object sender, System.EventArgs e)
+        {
+            int weight = Int32.Parse(Weight.Text);
+            int height = Int32.Parse(Height.Text);
+            int bmi = (weight * 703) / (height * height);
+            TheLabel.Text = $"Your BMI is {bmi}.";
+        }
+    }
+}
